5f5f5882162f14089957917f5b0c5e9445a7b4fb
Chris Tzatzakis <33452504+CTzatzakis@users.noreply.github.com>
2018-07-29 13:18:10 +0300

    update

---
+++ index.php
@@ -0,0 +1,190 @@
+<?php
+defined('_JEXEC') or die;
+
+/**
+ * Template for Joomla! CMS.
+ * See readme.txt for more details on how to use the template.
+ */
+
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'functions.php';
+
+// Create alias for $this object reference:
+$document = $this;
+
+// Shortcut for template base url:
+$templateUrl = $document->baseurl . '/templates/' . $document->template;
+
+Artx::load("Artx_Page");
+
+// Initialize $view:
+$view = $this->artx = new ArtxPage($this);
+
+// Decorate component with style:
+$view->componentWrapper();
+
+JHtml::_('behavior.framework', true);
+
+?>
+<!DOCTYPE html>
+<html dir="ltr" lang="<?php echo $document->language; ?>">
+<head>
+    <jdoc:include type="head" />
+    <link rel="stylesheet" href="<?php echo $document->baseurl; ?>/templates/system/css/system.css" />
+    <link rel="stylesheet" href="<?php echo $document->baseurl; ?>/templates/system/css/general.css" />
+    
+    <meta name="viewport" content="initial-scale = 1.0, maximum-scale = 1.0, user-scalable = no, width = device-width">
+
+    <!--[if lt IE 9]><script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <link rel="stylesheet" href="<?php echo $templateUrl; ?>/css/template.css" media="screen">
+    <!--[if lte IE 7]><link rel="stylesheet" href="<?php echo $templateUrl; ?>/css/template.ie7.css" media="screen" /><![endif]-->
+    <link rel="stylesheet" href="<?php echo $templateUrl; ?>/css/template.responsive.css" media="all">
+
+
+    <script>if ('undefined' != typeof jQuery) document._artxJQueryBackup = jQuery;</script>
+    <script src="<?php echo $templateUrl; ?>/jquery.js"></script>
+    <script>jQuery.noConflict();</script>
+
+    <script src="<?php echo $templateUrl; ?>/script.js"></script>
+    <?php $view->includeInlineScripts() ?>
+    <script>if (document._artxJQueryBackup) jQuery = document._artxJQueryBackup;</script>
+    <script src="<?php echo $templateUrl; ?>/script.responsive.js"></script>
+</head>
+<body>
+
+<div id="art-main">
+    <div id="art-header-bg">
+            </div>
+    <div class="art-sheet clearfix">
+<header class="art-header"><?php echo $view->position('position-30', 'art-nostyle'); ?>
+
+    <div class="art-shapes">
+        
+            </div>
+
+<h1 class="art-headline" data-left="85.66%">
+    <a href="<?php echo $document->baseurl; ?>/"><?php echo $this->params->get('siteTitle'); ?></a>
+</h1>
+<h2 class="art-slogan" data-left="88.31%"><?php echo $this->params->get('siteSlogan'); ?></h2>
+
+
+
+
+
+                
+                    
+</header>
+<?php if ($view->containsModules('position-1', 'position-28', 'position-29')) : ?>
+<nav class="art-nav">
+    
+<?php if ($view->containsModules('position-28')) : ?>
+<div class="art-hmenu-extra1"><?php echo $view->position('position-28'); ?></div>
+<?php endif; ?>
+<?php if ($view->containsModules('position-29')) : ?>
+<div class="art-hmenu-extra2"><?php echo $view->position('position-29'); ?></div>
+<?php endif; ?>
+<?php echo $view->position('position-1'); ?>
+ 
+    </nav>
+<?php endif; ?>
+<?php echo $view->position('position-15', 'art-nostyle'); ?>
+<?php echo $view->positions(array('position-16' => 33, 'position-17' => 33, 'position-18' => 34), 'art-block'); ?>
+<div class="art-layout-wrapper">
+                <div class="art-content-layout">
+                    <div class="art-content-layout-row">
+                        <?php if ($view->containsModules('position-7', 'position-4', 'position-5')) : ?>
+<div class="art-layout-cell art-sidebar1">
+<?php echo $view->position('position-7', 'art-block'); ?>
+<?php echo $view->position('position-4', 'art-block'); ?>
+<?php echo $view->position('position-5', 'art-block'); ?>
+
+
+
+
+                        </div>
+<?php endif; ?>
+
+                        <div class="art-layout-cell art-content">
+<?php
+  echo $view->position('position-19', 'art-nostyle');
+  if ($view->containsModules('position-2'))
+    echo artxPost($view->position('position-2'));
+  echo $view->positions(array('position-20' => 50, 'position-21' => 50), 'art-article');
+  echo $view->position('position-12', 'art-nostyle');
+  echo artxPost(array('content' => '<jdoc:include type="message" />', 'classes' => ' art-messages'));
+  echo '<jdoc:include type="component" />';
+  echo $view->position('position-22', 'art-nostyle');
+  echo $view->positions(array('position-23' => 50, 'position-24' => 50), 'art-article');
+  echo $view->position('position-25', 'art-nostyle');
+?>
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+<?php echo $view->positions(array('position-9' => 33, 'position-10' => 33, 'position-11' => 34), 'art-block'); ?>
+<?php echo $view->position('position-26', 'art-nostyle'); ?>
+
+<footer class="art-footer">
+<div class="art-content-layout layout-item-0">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-1" style="width: 20%">
+<?php if ($view->containsModules('position-31')) : ?>
+    <?php echo $view->position('position-31', 'art-nostyle'); ?>
+<?php else: ?>
+        <p style="font: 18px Arial; color: #444444">INFO</p>
+        <br>
+        <ul>
+        <li><a href="#">Welcome</a></li>
+        <li><a href="#">People</a></li>
+        <li><a href="#">Management</a></li>
+        </ul>
+    <?php endif; ?>
+</div><div class="art-layout-cell layout-item-1" style="width: 20%">
+<?php if ($view->containsModules('position-32')) : ?>
+    <?php echo $view->position('position-32', 'art-nostyle'); ?>
+<?php else: ?>
+        <p style="font: 18px Arial; color: #444444">LOCATION</p>
+        <br>
+        <ul>
+        <li><a href="#">Map</a></li>
+        <li><a href="#">Address</a></li>
+        <li><a href="#">Contact Us</a></li>
+        </ul>
+    <?php endif; ?>
+</div><div class="art-layout-cell layout-item-1" style="width: 20%">
+<?php if ($view->containsModules('position-33')) : ?>
+    <?php echo $view->position('position-33', 'art-nostyle'); ?>
+<?php else: ?>
+        <p style="font: 18px Arial; color: #444444">ABOUT</p>
+        <br>
+        <ul>
+        <li><a href="#">Company</a></li>
+        <li><a href="#">Terms</a></li>
+        </ul>
+    <?php endif; ?>
+</div><div class="art-layout-cell layout-item-1" style="width: 40%">
+<?php if ($view->containsModules('position-34')) : ?>
+    <?php echo $view->position('position-34', 'art-nostyle'); ?>
+<?php else: ?>
+        <p style="text-align:right"><br>
+        <br>Energy First © 2014-Present</p>
+    <?php endif; ?>
+</div>
+    </div>
+</div>
+
+</footer>
+
+    </div>
+    <p class="art-page-footer">
+        <span id="art-footnote-links"> Created by <a href="http://www.facebook.com/chris.tzatzakis" target="_blank">Chris Tzatzakis</a>.</span>
+    </p>
+</div>
+
+
+
+<?php echo $view->position('debug'); ?>
+</body>
+</html>
